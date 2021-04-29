e38b5aa786069cc551a15bfc2da1020c2c7b8327
Michael Gay <michael.mg.09a@gmail.com>
2021-04-29 12:26:34 +0200

    renommage fichier chess_tournament en main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#! usr/bin/env python3
+# coding:utf-8
+
+import view
+import controller
+
+"""Docstring"""
+
+
+def main():
+    view.ShowMenu.menu()
+    controller.Menu.choice_menu()
+
+
+if __name__ == "__main__":
+    main()
