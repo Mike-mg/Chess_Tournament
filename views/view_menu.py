3b21fdcda502670c23b0ca6ec84a6fa453718409
mike-mg <michael.mg.09a@gmail.com>
2021-05-19 22:41:33 +0200

    update views/view_menu.py

--- views/view_menu.py
+++ views/view_menu.py
@@ -4,10 +4,7 @@
 import os
 import sys
 
-import views.view_tournament
-import views.view_players
-import controllers.controller_tournament
-import controllers.controller_player
+import controllers.controller_menu
 
 
 class ShowMenu:
@@ -22,24 +19,19 @@ class ShowMenu:
         elif sys.platform.startswith('darwin'):
             os.system('clear')
 
-        menu_list = ['Add a player',
-                     'List of all players',
-                     'Create a tournament',
-                     'Show tournament',
-                     'Modified current tournaments',
-                     'Remove a player',
-                     'Return menu',
+        menu_list = ['List of all players',
+                     'Add a player',
                      'Modified ranking player',
+                     'Remove a player',
+                     'List of all tournaments',
+                     'Add a tournament',
+                     'Modified a tournaments',
+                     'Return menu',
                      'Leave -*- CHESS TOURNAMENT -*-'
                      ]
 
-        print(f"\n\n{'=' * 119}\n"
-              f"{'|'}{'-*- CHESS TOURNAMENT -*-'.center(117)}{'|'}\n"
-              f"{'=' * 119}\n\n")
-
-        print(f"{'=' * 119}\n"
-              f"{'|'}{'* MENU *'.center(117)}{'|'}\n"
-              f"{'-' * 119}")
+        ShowMenu.sub_menu('*** -*- WELCOME TO THE CHESS TOURNAMENT PROGRAM -*- ***')
+        ShowMenu.sub_menu('* MENU *')
 
         for index, m_menu in enumerate(menu_list):
             print(f"{':: '}{index} > {m_menu}")
@@ -51,8 +43,8 @@ class ShowMenu:
 
         while 1:
 
-            choice = input(f"\n{'=' * 22}\n"
-                           f"Help : 6 > Return menu"
+            choice = input(f"\n\n\n{'=' * 22}\n"
+                           f"Help : 7 > Return menu"
                            f"\n{'-' * 22}"
                            f"\n{':: Select a option > '}")
 
@@ -60,42 +52,17 @@ class ShowMenu:
                 choice = int(choice)
 
             except ValueError:
-                print(f"\n{':: ERROR > Enter a valid option [0-6]'}\n")
+                print(f"\n{':: ERROR > Enter a valid option [0-8]'}\n")
                 continue
 
-            if choice == 0:
-                # Add a player
-                controllers.controller_player.ControllerAddPlayer.controller_add_player()
+            controllers.controller_menu.ChoiceMenu.choice_user_menu(choice)
 
-            elif choice == 1:
-                # Show all player
-                views.view_players.ViewPlayer.show()
+    @staticmethod
+    def sub_menu(title):
+        print(f"\n\n\n{'=' * 119}\n"
+              f"{title.center(119)}\n"
+              f"{str('-' * len(title)).center(119)}\n\n")
 
-            elif choice == 2:
-                # Create a tournament
-                controllers.controller_tournament.ControllerAddTournament.controller_add_tournament()
-
-            elif choice == 3:
-                # Show current tournament
-                controllers.controller_tournament.ControllerAddTournament.show_tournament()
-
-            elif choice == 4:
-                # Modified current tournament
-                controllers.controller_tournament.ControllerAddTournament.modified_tournament()
-
-            elif choice == 5:
-                controllers.controller_player.ControllerAddPlayer.controller_player_remove()
-
-            elif choice == 6:
-                # show menu
-                ShowMenu.menu()
-
-            elif choice == 7:
-                controllers.controller_player.ControllerAddPlayer.controller_player_ranking()
-
-
-            elif choice in [8, 9]:
-                print(f"\n:: ERROR > Enter a valid option")
-
-            else:
-                break
+    @staticmethod
+    def get_string_value(get_string):
+        return input(f"- {get_string}: ")
