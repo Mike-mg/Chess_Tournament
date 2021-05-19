627b1f3dbfeecacee777a88eee17840079fbfbea
mike-mg <michael.mg.09a@gmail.com>
2021-05-19 22:41:43 +0200

    update views/view_players.py

--- views/view_players.py
+++ views/view_players.py
@@ -4,41 +4,41 @@
 """
 Modules views
 """
-
+import json
 from tinydb import Query
 import datetime
 
 import views.view_menu
-import models.models
+import models.models_players
 
 
 class ViewPlayer:
+
+    all_players = models.models_players.DeserializePlayer.players(models.models_players.PLAYERS.all())
 
     @staticmethod
     def show():
 
         views.view_menu.ShowMenu.menu()
 
-        print(f"\n\n{'=' * 119}\n"
-              f"{'|'}{'* List of all players *'.center(117)}{'|'}\n"
-              f"{'-' * 119}\n\n"
-              f"{'ID'.center(10)} | "
+        views.view_menu.ShowMenu.sub_menu('* List of all players *')
+
+        print(f"{'ID'.center(10)} | "
               f"{'Last name'.center(25)} | "
               f"{'Name'.center(25)} | "
               f"{'Birthday'.center(20)} | "
               f"{'Sex'.center(10)} | "
               f"{'Ranking'.center(10)}"
-              f"\n{'~' * 119}")
+              f"\n{'°' * 119}")
 
-        for player in models.models.PLAYERS:
-            print(f"{str(player.doc_id).center(10)} | "
+        for key, player in enumerate(ViewPlayer.all_players):
+            print(f"{str(key).center(10)} | "
                   f"{player['Last_name'].center(25)} | "
                   f"{player['Name'].center(25)} | "
                   f"{player['Birthday'].center(20)} | "
                   f"{player['Sex'].center(10)} | "
-                  f"{str(player['Ranking']).center(10)}")
-
-        print(f"\n{'-' * 119}")
+                  f"{str(player['Ranking']).center(10)}"
+                  f"\n{'-' * 119}")
 
     @staticmethod
     def add():
@@ -49,20 +49,19 @@ class ViewPlayer:
 
         while 1:
 
-            print(f"\n\n{'=' * 119}\n"
-                  f"{'|'}{'* Add A Player *'.center(117)}{'|'}\n"
-                  f"{'-' * 119}")
+            views.view_menu.ShowMenu.sub_menu('* Add A Player *')
 
             try:
-                last_name = input('- Last name : ').capitalize()
-                name = input('- Name : ').capitalize()
+                last_name = views.view_menu.ShowMenu.get_string_value('Last name').capitalize()
+                name = views.view_menu.ShowMenu.get_string_value('Name').capitalize()
                 print(f"\n{'Birth date'}\n"
                       f"{'-' * 10}")
-                year = int(input("- Year : "))
-                month = int(input("- Month : "))
-                day = int(input("- Day : "))
+                year = int(views.view_menu.ShowMenu.get_string_value('Year'))
+                month = int(views.view_menu.ShowMenu.get_string_value('Month'))
+                day = int(views.view_menu.ShowMenu.get_string_value('Day'))
                 birth_day = datetime.date(year, month, day)
-                sex = input('\n- Sex [F/M] : ').capitalize()
+                print()
+                sex = views.view_menu.ShowMenu.get_string_value('Sex [F/M]').capitalize()
 
                 if sex not in "FM":
                     print(f"""\n{":: ERROR > Enter a valid form. Ex: 'F' for female and 'M' for male for Sex"}\n""")
@@ -79,7 +78,7 @@ class ViewPlayer:
             ranking = f"{input('- Ranking : ')}"
 
             try:
-                ranking = int(ranking)
+                ranking = int(views.view_menu.ShowMenu.get_string_value(ranking))
 
             except ValueError:
                 print(f"\n{':: ERROR > Enter a valid format, the rank must be a positive number'}\n")
@@ -105,30 +104,36 @@ class ViewPlayer:
         choice_player = input('Select the player ID to be deleted : ')
         choice_player = int(choice_player)
 
-        id_player = models.models.PLAYERS.get(doc_id=choice_player)
+        id_player = models.models_players.PLAYERS.get(doc_id=choice_player)
 
-        models.models.PLAYERS.remove(player_q.Last_name == id_player["Last_name"])
+        print(f"\nRemove the next player\n"
+              f"{'-' * 22}")
+
+        for player in id_player.values():
+            print(f"- {player}")
+
+        accept_delete = input('\nDelete player [o/N] : ')
+
+        if accept_delete and accept_delete in "Oo":
+            models.models_players.PLAYERS.remove(player_q.Last_name == id_player["Last_name"])
 
         views.view_menu.ShowMenu.menu()
 
     @staticmethod
-    def ranking():
+    def new_ranking():
 
         views.view_players.ViewPlayer.show()
-
-        ranking_q = Query()
 
         id_player = input('Select player ID to change rank : ')
         id_player = int(id_player)
 
-        player = models.models.PLAYERS.get(doc_id=id_player)
+        for player_id, player in enumerate(ViewPlayer.all_players):
+            if id_player == player_id:
+                new_ranking = input('New ranking : ')
+                new_ranking = int(new_ranking)
+                player['Ranking'] = new_ranking
 
-        new_ranking = input('New ranking : ')
-        new_ranking = int(new_ranking)
-
-        models.models.PLAYERS.update({'Ranking': new_ranking}, ranking_q.Ranking == player['Ranking'])
-
-        views.view_players.ViewPlayer.show()
+        views.view_menu.ShowMenu.menu()
 
 
 if __name__ == "__main__":
