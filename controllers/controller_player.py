3b06ad378bcf911a718c819c663ec717ebb864d1
mike-mg <michael.mg.09a@gmail.com>
2021-05-22 12:22:52 +0200

    update controllers/controller_player.py

--- controllers/controller_player.py
+++ controllers/controller_player.py
@@ -10,33 +10,64 @@ import views.view_players
 import models.models_players
 
 
+class ControllerShowPlayers:
+    """
+        Class for the control show players
+    """
+
+    def __init__(self):
+        """
+        Shows all players
+        """
+
+        table_all_players = views.view_players.ViewShowPlayer(models.models_players.table_players.all())
+        table_all_players.show_players()
+
+
 class ControllerAddPlayer:
-
     """
-    Class ControllerAddPlayer
+    Class for the control add player
     """
 
-    @staticmethod
-    def controller_add_player():
+    def __init__(self):
         """
         Add player control
         """
 
-        player = views.view_players.ViewPlayer.add()
-        models.models_players.SerializedPlayer(player["last_name"],
-                                               player["name"],
-                                               player["birth_day"],
-                                               player["sex"],
-                                               player["ranking"])
+        add_player = views.view_players.ViewAddPlayer()
 
-    @staticmethod
-    def controller_player_remove():
-        views.view_players.ViewPlayer.remove()
+        player = models.models_players.Player(add_player.last_name,
+                                              add_player.name,
+                                              add_player.birthday,
+                                              add_player.sex,
+                                              add_player.ranking)
 
-    @staticmethod
-    def controller_player_ranking():
-        views.view_players.ViewPlayer.new_ranking()
+        player.serialized_player()
 
-    @staticmethod
-    def controller_player_deserialize():
-        views.view_players.ViewPlayer.show()
+
+class ControllerRemovePlayer:
+    """
+        Class for the control remove player
+    """
+
+    def __init__(self):
+
+        id_int = views.view_players.ViewRemovePlayer()
+        id_int.remove_player()
+
+        id_player = models.models_players.Remove(id_int.id_player_remove)
+        id_player.remove()
+
+
+class ControllerRankingPlayer:
+    """
+        Class for the new ranking player
+    """
+
+    def __init__(self):
+
+        ranking = views.view_players.ViewRankingPlayer()
+        ranking.new_ranking()
+
+        new_ranking_player = models.models_players.NewRanking(ranking.player_id, ranking.ranking)
+        new_ranking_player.new_ranking()
