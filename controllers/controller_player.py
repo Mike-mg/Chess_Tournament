a07c62b23e0a918680c8054afef5456976298d87
mike-mg <michael.mg.09a@gmail.com>
2021-05-29 11:41:37 +0200

    update controllers/controller_player.py

--- controllers/controller_player.py
+++ controllers/controller_player.py
@@ -8,66 +8,43 @@ Modules Models
 
 import views.view_players
 import models.models_players
+import views.view_menu
 
 
-class ControllerShowPlayers:
+def format_string(get_string):
+    return input(f"{get_string}")
+
+
+def controller_show_player():
     """
-        Class for the control show players
-    """
-
-    def __init__(self):
-        """
-        Shows all players
-        """
-
-        table_all_players = views.view_players.ViewShowPlayer(models.models_players.table_players.all())
-        table_all_players.show_players()
-
-
-class ControllerAddPlayer:
-    """
-    Class for the control add player
+    Shows all players
     """
 
-    def __init__(self):
-        """
-        Add player control
-        """
-
-        add_player = views.view_players.ViewAddPlayer()
-
-        player = models.models_players.Player(add_player.last_name,
-                                              add_player.name,
-                                              add_player.birthday,
-                                              add_player.sex,
-                                              add_player.ranking)
-
-        player.serialized_player()
+    views.view_players.view_show_player(models.models_players.table_players.all())
 
 
-class ControllerRemovePlayer:
+def controller_add_player():
     """
-        Class for the control remove player
+    Add player control
     """
 
-    def __init__(self):
+    list_players_tournament = list()
 
-        id_int = views.view_players.ViewRemovePlayer()
-        id_int.remove_player()
+    for player in views.view_players.view_add_player():
+        list_players_tournament.append(player)
 
-        id_player = models.models_players.Remove(id_int.id_player_remove)
-        id_player.remove()
+    print(list_players_tournament)
 
 
-class ControllerRankingPlayer:
-    """
-        Class for the new ranking player
-    """
-
-    def __init__(self):
-
-        ranking = views.view_players.ViewRankingPlayer()
-        ranking.new_ranking()
-
-        new_ranking_player = models.models_players.NewRanking(ranking.player_id, ranking.ranking)
-        new_ranking_player.new_ranking()
+# class ControllerRankingPlayer:
+#     """
+#         Class for the new ranking player
+#     """
+#
+#     def __init__(self):
+#
+#         ranking = views.view_players.ViewRankingPlayer()
+#         ranking.new_ranking()
+#
+#         new_ranking_player = models.models_players.new_rankin(ranking.player_id, ranking.ranking)
+#         new_ranking_player.new_ranking()
