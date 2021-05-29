c8afacf0aa8a734148ac8b1e220821e50eb6b9e7
mike-mg <michael.mg.09a@gmail.com>
2021-05-29 11:42:31 +0200

    update models/models_tournaments.py

--- models/models_tournaments.py
+++ models/models_tournaments.py
@@ -13,7 +13,17 @@ class Tournament:
     create a object Tournament
     """
 
-    def __init__(self, name, location, start_date, end_date, players, nb_rounds, tours):
+    def __init__(self,
+                 name,
+                 location,
+                 start_date,
+                 end_date,
+                 nb_rounds,
+                 tours,
+                 players,
+                 time_control,
+                 description
+                 ):
 
         tournament = {}
 
@@ -22,15 +32,19 @@ class Tournament:
         self.start_date = start_date
         self.end_date = end_date
         self.nb_rounds = nb_rounds
-        self.players = players
         self.tours = tours
+        self.players = players
+        self.time_control = time_control
+        self.description = description
 
         tournament['Name'] = self.name
         tournament['Location'] = self.location
         tournament['Start date'] = self.start_date
         tournament['End date'] = self.end_date
-        tournament['Players'] = self.players
         tournament['Nb_rounds'] = self.nb_rounds
         tournament['Tours'] = self.tours
+        tournament['Players'] = self.players
+        tournament['Time_control'] = self.time_control
+        tournament['Description'] = self.description
 
         TOURNAMENTS.insert(tournament)
