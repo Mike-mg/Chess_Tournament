ada4a3cb7e4033de6a737fd94b5dd3a61ad8528e
mike-mg <michael.mg.09a@gmail.com>
2021-05-29 11:42:44 +0200

    update views/view_menu.py

--- views/view_menu.py
+++ views/view_menu.py
@@ -7,71 +7,67 @@ import sys
 import controllers.controller_menu
 
 
-class ShowMenu:
-    '''
+def show_menu():
+    """
     Docstrings
-    '''
+    """
 
-    def __init__(self):
+    if sys.platform.startswith('linux'):
+        os.system('clear')
+    elif sys.platform.startswith('win32'):
+        os.system('cls')
+    elif sys.platform.startswith('darwin'):
+        os.system('clear')
 
-        if sys.platform.startswith('linux'):
-            os.system('clear')
-        elif sys.platform.startswith('win32'):
-            os.system('cls')
-        elif sys.platform.startswith('darwin'):
-            os.system('clear')
+    menu_list = ['List of all players',
+                 'Add a player',
+                 'Modified ranking player',
+                 'List of all tournaments',
+                 'Add a tournament',
+                 'Modified a tournaments',
+                 'Return menu',
+                 'Leave -*- CHESS TOURNAMENT -*-'
+                 ]
 
-        menu_list = ['List of all players',
-                     'Add a player',
-                     'Modified ranking player',
-                     'Remove a player',
-                     'List of all tournaments',
-                     'Add a tournament',
-                     'Modified a tournaments',
-                     'Return menu',
-                     'Leave -*- CHESS TOURNAMENT -*-'
-                     ]
+    sub_menu('/^\ *** -*- WELCOME TO THE CHESS TOURNAMENT PROGRAM -*- *** /^\\')
+    sub_menu('* MENU *')
 
-        SubMenu('/^\ *** -*- WELCOME TO THE CHESS TOURNAMENT PROGRAM -*- *** /^\\')
-        SubMenu('* MENU *')
+    for index, m_menu in enumerate(menu_list):
+        print(f"[ {index} ]  {m_menu}")
 
-        for index, m_menu in enumerate(menu_list):
-            print(f"{':: '}{index} > {m_menu}")
-
-        print(f"{'-' * 119}")
+    print(f"{'-' * 119}")
 
 
-class ChoiceMenu():
-    '''
+def choice_menu():
+    """
     Docstrings
-    '''
+    """
 
-    def __init__(self):
+    while 1:
 
-        while 1:
+        choice = input(f"\n\n\n{'=' * 22}\n"
+                       f"Help : 7 > Return menu"
+                       f"\n{'-' * 22}"
+                       f"\n{':: Select a option > '}")
 
-            choice = input(f"\n\n\n{'=' * 22}\n"
-                           f"Help : 7 > Return menu"
-                           f"\n{'-' * 22}"
-                           f"\n{':: Select a option > '}")
+        try:
+            choice = int(choice)
 
-            try:
-                choice = int(choice)
+        except ValueError:
+            print(f"\n{':: ERROR > Enter a valid option [0-8]'}\n")
+            continue
 
-            except ValueError:
-                print(f"\n{':: ERROR > Enter a valid option [0-8]'}\n")
-                continue
+        if controllers.controller_menu.choice_user_menu(choice) is False:
+            break
 
-            controllers.controller_menu.ChoiceMenu.choice_user_menu(choice)
+    controllers.controller_menu.choice_user_menu(choice)
 
 
-class SubMenu:
-    '''
+def sub_menu(title):
+    """
     Docstrings
-    '''
+    """
 
-    def __init__(self,title):
-
-        print(f"\n\n\n{'=' * 119}\n"
-              f"{title.center(119)}\n"
-              f"{str('-' * len(title)).center(119)}\n\n")
+    print(f"\n\n\n{'=' * 119}\n"
+          f"{title.center(119)}\n"
+          f"{str('-' * len(title)).center(119)}\n\n")
