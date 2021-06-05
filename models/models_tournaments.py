5f9f208efc9fe994adea15dfb3e80350bd4b8752
mike-mg <michael.mg.09a@gmail.com>
2021-06-05 12:19:44 +0200

    update models/models_tournaments.py

--- models/models_tournaments.py
+++ models/models_tournaments.py
@@ -3,29 +3,20 @@
 
 from tinydb import TinyDB
 
+import views.view_players
+import models.models_players
+
 DB_TOURNAMENTS = TinyDB('Bdd/db_tournaments.json', indent=4)
 TOURNAMENTS = DB_TOURNAMENTS.table("tournaments")
 
 
 class Tournament:
-
     """
     create a object Tournament
     """
 
-    def __init__(self,
-                 name,
-                 location,
-                 start_date,
-                 end_date,
-                 nb_rounds,
-                 tours,
-                 players,
-                 time_control,
-                 description
-                 ):
-
-        tournament = {}
+    def __init__(self, name, location, start_date, end_date, nb_rounds,
+                 tours, players, time_control, description):
 
         self.name = name.title()
         self.location = location.title()
@@ -37,14 +28,10 @@ class Tournament:
         self.time_control = time_control
         self.description = description
 
-        tournament['Name'] = self.name
-        tournament['Location'] = self.location
-        tournament['Start date'] = self.start_date
-        tournament['End date'] = self.end_date
-        tournament['Nb_rounds'] = self.nb_rounds
-        tournament['Tours'] = self.tours
-        tournament['Players'] = self.players
-        tournament['Time_control'] = self.time_control
-        tournament['Description'] = self.description
+    def match(self):
 
-        TOURNAMENTS.insert(tournament)
+        for k, v in enumerate(models.models_players.deserialized_table_players()):
+            print(k, v)
+
+        for i in self.players:
+            print(i)
