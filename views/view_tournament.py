44cd68425033d5e9b31f5dc26b1d633d126e16e0
mike-mg <michael.mg.09a@gmail.com>
2021-06-12 11:45:06 +0200

    update views/view_tournament.py

--- views/view_tournament.py
+++ views/view_tournament.py
@@ -6,10 +6,11 @@ import views
 """
 
 import datetime
+import operator
 
 import views.view_players
 import views.view_menu
-
+import models.models_tournaments
 
 def format_string(get_string):
     return input(f":: {get_string} > ")
@@ -17,59 +18,48 @@ def format_string(get_string):
 
 class ViewTournament:
     """
-    Objet tournament
+    Objet view tournament
     """
 
-    def __init__(self):
+    def menu_tournament(self, list_tournament):
+        """
+        Show menu tournament
+        """
 
-        # self.name = str()
-        # self.location = str()
-        # self.start_date = str()
-        # self.end_date = str()
-        # self.nb_rounds = 4
-        # self.tours = list()
-        # self.players = []
-        # self.time_control = str()
-        # self.description = str()
+        # views.view_menu.show_menu()
+        views.view_menu.sub_menu("Current tournament")
 
-        self.name = str("chess 1")
-        self.location = str("France")
-        self.start_date = str("01/01/2021")
-        self.end_date = str("01/01/2021")
-        self.nb_rounds = 4
-        self.tours = list()
-        self.players = [1, 2, 3, 4, 5, 6, 7, 8]
-        self.time_control = str("Bullet")
-        self.description = str("Tournament of 01/01/2021 in France ")
+        for key, value in enumerate(list_tournament):
+            print(f"[ {key} ] - [ {value.name}, {value.location} ]")
 
     def main_tournament_info(self):
         """
         Name, location, date, description and time control of tournament
         """
 
-        views.view_menu.show_menu()
+        # views.view_menu.show_menu()
         views.view_menu.sub_menu('* Add A Tournament *')
 
-        self.name = format_string("Name tournament").capitalize()
-        self.location = format_string("Location").capitalize()
-        self.description = format_string("Description").capitalize()
+        name = format_string("Name tournament").capitalize()
+        location = format_string("Location").capitalize()
+        description = format_string("Description").capitalize()
 
-        date_string = [f"\nEnter start date\n{'-' * 16}",
-                       f"\nEnter end date\n{'-' * 14}"]
+        date_string = (f"\nEnter start date\n{'-' * 16}",
+                       f"\nEnter end date\n{'-' * 14}")
 
         date = []
 
-        for i in range(2):
+        for value in date_string:
 
-            print(date_string[i])
+            print(value)
 
             year = int(format_string("Year"))
             month = int(format_string("Month"))
             day = int(format_string("Day"))
             date.append(datetime.date(year, month, day).strftime("%d/%m/%Y"))
 
-        self.start_date = date[0]
-        self.end_date = date[1]
+        start_date = date[0]
+        end_date = date[1]
 
         list_time_control = ["Blitz", "Bullet", "Quick hit"]
 
@@ -80,14 +70,9 @@ class ViewTournament:
 
         choice_time_control = int(format_string("Choice time control"))
 
-        self.time_control = list_time_control[choice_time_control]
+        time_control = list_time_control[choice_time_control]
 
-    def tours_tournament(self):
-        """
-        Tours of tournament
-        """
-        for i in range(self.nb_rounds):
-            self.tours.append([])
+        return name, location, start_date, end_date, time_control, description
 
     def add_player_tournament(self):
         """
@@ -99,23 +84,80 @@ class ViewTournament:
         print(f"\n{'Select players by their index to add them to the tournament'}\n"
               f"{'-' * 59}")
 
+        players = []
+
         for i in range(8):
+
             player = int(format_string(f"Player {i + 1}"))
-            self.players.append(player)
 
-    def show_tournament(self, tournament):
+            players.append(player)
 
-        views.view_menu.sub_menu(f"Tournament {tournament.name}")
+        return players
 
-        print(f"{':: Name tournament':<20}{'> '}{tournament.name}\n"
-              f"{':: Location':<20}{'> '}{tournament.location}\n"
-              f"{':: Start date':<20}{'> '}{tournament.start_date}\n"
-              f"{':: End date':<20}{'> '}{tournament.end_date}\n"
-              f"{':: Nb rounds':<20}{'> '}{tournament.nb_rounds}\n"
-              f"{':: Tours':<20}{'> '}{tournament.tours}\n"
-              f"{':: Players':<20}{'> '}{tournament.players}\n"
-              f"{':: Time control':<20}{'> '}{tournament.time_control}\n"
-              f"{':: Description':<20}{'> '}{tournament.description}")
+    def show_tournament(self, list_tournament):
+
+        self.menu_tournament(list_tournament)
+
+        select_tournament = int(input(f"\n{'-' * 37}\n{':: Select the tournament to be show > '}"))
+
+        for key, tournament in enumerate(list_tournament):
+            if key == select_tournament:
+
+                views.view_menu.sub_menu(tournament.name)
+
+                print(f"{':: Number tournament':<22}{'> '}{key}\n"
+                      f"{':: Name tournament':<22}{'> '}{tournament.name}\n"
+                      f"{':: Location':<22}{'> '}{tournament.location}\n"
+                      f"{':: Start date':<22}{'> '}{tournament.start_date}\n"
+                      f"{':: End date':<22}{'> '}{tournament.end_date}\n"
+                      f"{':: Nb rounds':<22}{'> '}{tournament.nb_rounds}\n"
+                      f"{':: Players':<22}{'> '}{tournament.players}\n"
+                      f"{':: Time control':<22}{'> '}{tournament.time_control}\n"
+                      f"{':: Description':<22}{'> '}{tournament.description}\n"
+                      f"\n{'=' * 119}\n"
+                      f"{'Tours'.center(119)}\n"
+                      f"{'-' * 119}")
+
+                for all_round in tournament.tours:
+                    for key, value in all_round.items():
+                        print(f"\n\n[ {key} ]\n{'-' * 11}")
+
+                        i = 0
+                        for round in value:
+                            print(f":: Match {i + 1} > Player N°{round[0][0]}:{round[0][2]} Vs "
+                                  f"N°{round[1][0]}:{round[1][2]}")
+                            i += 1
+
+    def round_2(self, list_tournament):
+
+        results_round_1 = []
+        key_tournament = int()
+
+        select_tournament = int(input(f"\n{'-' * 41}\n{':: Select the tournament to be modified > '}"))
+
+        for key, value in enumerate(list_tournament):
+            if key == select_tournament:
+                key_tournament = key
+                views.view_menu.sub_menu(f"[ Tournament : {value.name} ]  {'Round 1 results'}")
+
+                print(f"{'Enter the points of the players in round 1'}\n{'-' * 42}")
+
+                for number_match, match in enumerate(value.tours[0]['Round_1']):
+
+                    print(f"\n[ Match {number_match + 1} ] Player N°{list(match[0])[0]} Vs N°{list(match[1])[0]}\n{'-' * 29}")
+
+                    new_point_match_player_1 = int(format_string(f"New point player N°{match[0][0]}"))
+                    new_point_match_player_2 = int(format_string(f"New point player N°{match[1][0]}"))
+
+                    match[0], match[1] = list(match[0]), list(match[1])
+
+                    match[0][2], match[1][2] = new_point_match_player_1, new_point_match_player_2
+
+                    match[0], match[1] = tuple(match[0]), tuple(match[1])
+
+                    results_round_1.append(match)
+
+        return key_tournament, results_round_1
 
 
 if __name__ == "__main__":
