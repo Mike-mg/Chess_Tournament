27a5be426a61a36f6d9297b75e09427c17d21fe9
mike-mg <michael.mg.09a@gmail.com>
2021-09-01 18:31:55 +0200

    update models_players.py

--- models/models_players.py
+++ models/models_players.py
@@ -10,11 +10,13 @@ table_players = db_players.table("players")
 
 class Player:
     """
-    Serialize and Create a object Player
+    Class that creates player objects
     """
 
-    def __init__(self, last_name, name, birthday, sex, ranking, points=0):
-
+    def __init__(self, last_name: str, name: str, birthday: str, sex: str, ranking: int, points: int = 0):
+        """
+        Builds the player object
+        """
         self.last_name = last_name.capitalize()
         self.name = name.capitalize()
         self.birthday = birthday
@@ -22,31 +24,31 @@ class Player:
         self.ranking = ranking
         self.point = points
 
+    def deserialized_table_players(self, table_list_players=table_players):
+        """
+        Deserialized table players
+        """
+
+        all_players = list()
+
+        for player in table_list_players:
+            all_players.append(player)
+
+        return all_players
+
 
 def serialized_player(player):
 
     serial_player = dict()
 
-    serial_player['Last_name'] = player[0]
-    serial_player['Name'] = player[1]
-    serial_player['Birthday'] = player[2]
-    serial_player['Sex'] = player[3]
-    serial_player['Ranking'] = player[4]
-    serial_player['Points'] = player[5]
+    serial_player['last_name'] = player[0]
+    serial_player['name'] = player[1]
+    serial_player['birthday'] = player[2]
+    serial_player['sex'] = player[3]
+    serial_player['ranking'] = player[4]
+    serial_player['points'] = player[5]
 
     table_players.insert(serial_player)
-
-
-def deserialized_table_players(table_list_players=table_players):
-    """
-    Deserialized table players
-    """
-    all_players = list()
-
-    for player in table_list_players:
-        all_players.append(player)
-
-    return all_players
 
 
 if __name__ == "__main__":
