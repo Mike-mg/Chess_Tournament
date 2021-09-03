35c33c817101bf88a4df4384d05355a63b419b32
mike-mg <michael.mg.09a@gmail.com>
2021-09-03 22:08:21 +0200

    update views/view_players.py

--- views/view_players.py
+++ views/view_players.py
@@ -1,56 +1,51 @@
 #! /usr/bin/env python3
 # coding:utf-8
 
-"""
-Modules views
-"""
-
 import views.view_menu
 import utility_functions.utils_func
-import models.models_players
-import bdd.db_serialized_deserialized_players_func
-
+import controllers.controller_player
 
 
 class ViewPlayer:
+    """
+    Show and retrieves player information
+    """
 
     def __init__(self):
 
         self.view_menu = views.view_menu.ViewMenu()
 
-    def view_show_player(self, table_players=bdd.db_serialized_deserialized_players_func.deserialized_table_players()):
+    def view_show_player(self):
         """
-        View show all player
+        Show all players
         """
-        print(table_players)
-        # all_players = table_players
 
-        # self.view_menu.show_menu()
-        # self.view_menu.sub_menu('* List of all players *')
-        #
-        # print(f"{'ID'.center(10)} | "
-        #       f"{'Last name'.center(20)} | "
-        #       f"{'Name'.center(20)} | "
-        #       f"{'Birthday'.center(15)} | "
-        #       f"{'Sex'.center(10)} | "
-        #       f"{'Ranking'.center(10)} | "
-        #       f"{'Points'.center(10)}"
-        #       f"\n{'°' * 119}")
-        #
-        # for key, player in enumerate(table_players):
-        #
-        #     print(f"{str(key).center(10)} | "
-        #           f"{player['Last_name'].center(20)} | "
-        #           f"{player['Name'].center(20)} | "
-        #           f"{player['Birthday'].center(15)} | "
-        #           f"{player['Sex'].center(10)} | "
-        #           f"{str(player['Ranking']).center(10)} | "
-        #           f"{str(player['Points']).center(10)}"
-        #           f"\n{'-' * 119}")
+        self.view_menu.show_menu()
+        self.view_menu.sub_menu('* List of all players *')
 
-    def add_player(self):
+        print(f"{'ID'.center(10)} | "
+              f"{'Last name'.center(20)} | "
+              f"{'Name'.center(20)} | "
+              f"{'Birthday'.center(15)} | "
+              f"{'Sex'.center(10)} | "
+              f"{'Ranking'.center(10)} | "
+              f"{'Points'.center(10)}"
+              f"\n{'°' * 119}")
+
+        for key, player in enumerate(controllers.controller_player.ControllerPlayer.ALL_PLAYERS):
+
+            print(f"{str(key).center(10)} | "
+                  f"{player.last_name.center(20)} | "
+                  f"{player.name.center(20)} | "
+                  f"{player.birthday.center(15)} | "
+                  f"{player.sex.center(10)} | "
+                  f"{str(player.ranking).center(10)} | "
+                  f"{str(player.points).center(10)}"
+                  f"\n{'-' * 119}")
+
+    def add_player(self) -> tuple:
         """
-        Docstrings
+        Add a player
         """
 
         self.view_menu.show_menu()
@@ -60,27 +55,29 @@ class ViewPlayer:
         name = utility_functions.utils_func.format_string("Name")
         birthday = utility_functions.utils_func.format_string("Birthday")
         sex = utility_functions.utils_func.format_string("Sex")
-        ranking = utility_functions.utils_func.format_string("Ranking")
+        ranking = int(utility_functions.utils_func.format_string("Ranking"))
         points = 0
 
         return last_name, name, birthday, sex, ranking, points
 
-    def new_player_ranking(self, players):
+    def new_player_ranking(self, all_players: list):
+        """
+        Modified ranking player
+        """
 
         self.view_show_player()
 
         select_player = int(input(f"\n{'-' * 41}\n{':: Select the player to be modified > '}"))
 
-        for key, value in enumerate(players):
+        for key, player in enumerate(all_players):
 
             if key == select_player:
-                print(f"\n{'Select player :'} {value['Last_name']} {value['Name']}\n{'-' * 30}")
+                print(f"\n{'Select player :'} {player.last_name} {player.name}\n{'-' * 30}")
 
                 new_ranking = int(input('Enter the new ranking : '))
 
-                value['Ranking'] = new_ranking
+                player.ranking = new_ranking
 
-                return key + 1, value
 
 if __name__ == "__main__":
     pass
