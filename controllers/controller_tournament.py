5ca296237e313e286916aa86b68cfffed5c913df
mike-mg <michael.mg.09a@gmail.com>
2021-09-10 22:28:45 +0200

    update controller_tournament.py

--- controllers/controller_tournament.py
+++ controllers/controller_tournament.py
@@ -3,14 +3,18 @@
 
 import views
 import models
+import bdd
 
 
 class ControllerTournament:
     """
     Controls the creation of tournaments
     """
-
+    # BDD_TOURNAMENTS = bdd.deserialized_table_tournament()
     TOURNAMENTS = []
+    
+    BDD_PLAYERS = bdd.deserialized_table_players()
+    BDD_TOURNAMENTS = bdd.deserialized_table_tournament()
 
     def __init__(self):
 
@@ -24,7 +28,7 @@ class ControllerTournament:
 
         # main_info_tournament = self.view_tournament.main_tournament_info()
         # add_player = self.view_tournament.add_player_tournament()
-
+        
         # tournament = models.models_tournaments.Tournament(main_info_tournament[0],
         #                                                   main_info_tournament[1],
         #                                                   main_info_tournament[2],
@@ -32,24 +36,34 @@ class ControllerTournament:
         #                                                   add_player,
         #                                                   main_info_tournament[4],
         #                                                   main_info_tournament[5])
-
+        
         tournament = models.Tournament("Tournament 1",
                                        "French",
                                        "01/01/2020",
                                        "01/01/2020",
-                                       [1, 2, 3, 4, 5, 6, 7, 8],
+                                       [ControllerTournament.BDD_PLAYERS[0],
+                                        ControllerTournament.BDD_PLAYERS[1],
+                                        ControllerTournament.BDD_PLAYERS[2],
+                                        ControllerTournament.BDD_PLAYERS[3],
+                                        ControllerTournament.BDD_PLAYERS[4],
+                                        ControllerTournament.BDD_PLAYERS[5],
+                                        ControllerTournament.BDD_PLAYERS[6],
+                                        ControllerTournament.BDD_PLAYERS[7]],
                                        "time control",
                                        "description")
 
-        tournament.round_1()
+
+        round_1 = tournament.round_1()
+        tournament.tours.insert(round_1[0], round_1[1])
         ControllerTournament.TOURNAMENTS.append(tournament)
+        bdd.serialized_tournament(ControllerTournament.TOURNAMENTS)
 
     def show_tournament(self):
         """
         Displays the general information of the tournament
         """
 
-        self.view_tournament.show_tournament(ControllerTournament.TOURNAMENTS)
+        self.view_tournament.show_tournament(ControllerTournament.BDD_TOURNAMENTS)
 
     def result_round(self):
         """
