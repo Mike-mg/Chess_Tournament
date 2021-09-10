820538ef48579aa4e4e07b4ea3234e7b753444b1
mike-mg <michael.mg.09a@gmail.com>
2021-09-10 22:28:25 +0200

    update __init__.py

--- bdd/__init__.py
+++ bdd/__init__.py
@@ -1,2 +1,4 @@
 from bdd.db_functions import serialized_player
 from bdd.db_functions import deserialized_table_players
+from bdd.db_functions import serialized_tournament
+from bdd.db_functions import deserialized_table_tournament
