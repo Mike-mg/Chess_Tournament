0d039ca85c73491f851faa4f0f6af598b69d00af
mike-mg <michael.mg.09a@gmail.com>
2021-09-10 22:29:32 +0200

    update db_functions.py

--- bdd/db_functions.py
+++ bdd/db_functions.py
@@ -8,8 +8,11 @@ import models
 DB_PLAYERS = TinyDB('bdd/db_players.json', indent=4)
 TABLE_PLAYERS = DB_PLAYERS.table("players")
 
+DB_TOURNAMENTS = TinyDB('bdd/db_tournaments.json', indent=4)
+TABLE_TOURNAMENTS = DB_TOURNAMENTS.table("tournaments")
 
-def deserialized_table_players() -> list[object]:
+
+def deserialized_table_players() -> list[models.Player]:
     """
     Deserialized object players in var all_players
     """
@@ -27,7 +30,7 @@ def deserialized_table_players() -> list[object]:
     return all_players
 
 
-def serialized_player(list_players_object: list):
+def serialized_player(list_players_object: list[models.Player]):
     """
     serialize the list of player objects in parameter
     """
@@ -36,20 +39,69 @@ def serialized_player(list_players_object: list):
 
     for player in list_players_object:
 
-        player_serialized = dict()
+        tournament_serialized = dict()
 
-        player_serialized['last_name'] = player.last_name
-        player_serialized['name'] = player.name
-        player_serialized['birthday'] = player.birthday
-        player_serialized['sex'] = player.sex
-        player_serialized['ranking'] = player.ranking
-        player_serialized['points'] = player.points
+        tournament_serialized['last_name'] = player.last_name
+        tournament_serialized['name'] = player.name
+        tournament_serialized['birthday'] = player.birthday
+        tournament_serialized['sex'] = player.sex
+        tournament_serialized['ranking'] = player.ranking
+        tournament_serialized['points'] = player.points
 
-        serialized_players_for_table_players.append(player_serialized)
+        serialized_players_for_table_players.append(tournament_serialized)
 
     TABLE_PLAYERS.truncate()
     TABLE_PLAYERS.insert_multiple(serialized_players_for_table_players)
 
 
+def deserialized_table_tournament() -> list:
+    """
+    Deserialized object tournament in var all_tournaments
+    """
+    pass
+    # all_tournaments = list()
+    #
+    # for tournament in TABLE_TOURNAMENTS.all():
+    #     all_tournaments.append(models.Tournament(tournament['name'],
+    #                                              tournament['location'],
+    #                                              tournament['start_date'],
+    #                                              tournament['end_date'],
+    #                                              tournament['players'],
+    #                                              tournament['time_control'],
+    #                                              tournament['description'],
+    #                                              tournament['nb_rounds'],
+    #                                              tournament['tours'])
+    #
+    # return all_tournaments
+
+
+def serialized_tournament(list_tournament_object: list):
+    """
+    serialize the list of tournament objects in parameter
+    """
+
+    tournament_serialized = dict()
+    serialized_tournament_for_table_tournaments = list()
+
+    for tournament in list_tournament_object:
+
+        tournament_serialized['name'] = tournament.name
+        tournament_serialized['location'] = tournament.location
+        tournament_serialized['start_date'] = tournament.start_date
+        tournament_serialized['end_date'] = tournament.end_date
+        tournament_serialized['players'] = tournament.players
+        tournament_serialized['time_control'] = tournament.time_control
+        tournament_serialized['description'] = tournament.description
+        tournament_serialized['nb_rounds'] = tournament.nb_rounds
+        tournament_serialized['tours'] = tournament.tours
+
+        serialized_tournament_for_table_tournaments.append(tournament_serialized)
+
+
+    TABLE_TOURNAMENTS.truncate()
+    TABLE_TOURNAMENTS.insert_multiple(serialized_tournament_for_table_tournaments)
+
+
 if __name__ == "__main__":
     pass
+
